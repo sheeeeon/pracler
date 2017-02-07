e0b9afa367c32fd6e4814cd65b59376ba0cd9389
icaynia <icaynia@icaynia.com>
2017-02-08 01:16:47 +0900

    Apply pattern

---
+++ app/src/main/java/com/icaynia/madori/Data/LocalDatabaseManager.java
@@ -0,0 +1,8 @@
+package com.icaynia.madori.Data;
+
+/**
+ * Created by icaynia on 2017. 2. 8..
+ */
+
+public class LocalDatabaseManager
+{}
---
+++ app/src/main/java/com/icaynia/madori/Data/RemoteDatabaseManager.java
@@ -0,0 +1,13 @@
+package com.icaynia.madori.Data;
+
+/**
+ * Created by icaynia on 2017. 2. 8..
+ *
+ * 파이어베이스 데이터베이스와 연동하는 부분
+ * 데이터베이스에 추가시키거나 가져와서 리스너에 연결시키는 부분을 여기서 다 한다.
+ */
+
+public class RemoteDatabaseManager
+{
+
+}
