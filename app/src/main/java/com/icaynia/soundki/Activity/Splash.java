36f00adb49eb1e65bf46adcf2502616d0527c6a4
icaynia <icaynia@icaynia.com>
2017-02-13 11:52:53 +0900

    Add activitys

---
+++ app/src/main/java/com/icaynia/soundki/Activity/Splash.java
@@ -0,0 +1,20 @@
+package com.icaynia.soundki.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.icaynia.soundki.R;
+
+/**
+ * Created by icaynia on 13/02/2017.
+ */
+
+public class Splash extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
