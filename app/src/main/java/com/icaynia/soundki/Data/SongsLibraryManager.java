e56c05f0755fccc3e37ffe625cc65662502c1662
Sion Yu <icaynia@icaynia.com>
2017-02-14 02:08:18 +0900

    Create SongsLibraryManager.java
    Add some method

---
+++ app/src/main/java/com/icaynia/soundki/Data/SongsLibraryManager.java
@@ -0,0 +1,26 @@
+package com.icaynia.soundki.Data;
+
+public class SongsLibraryManager
+{
+    public SongsLibraryManager()
+    {
+        
+    }
+    
+    public MusicDto getSongLibrary(String songUid)
+    {
+    
+    }
+    
+    public ArrayList<String> findSong(String title, String artist, String albumTitle, OnCompleteListener)
+    {
+        ArrayList<String> findUidList = new ArrayList<String>();
+        return findUidList;
+    }
+    
+    public void addSong(MusicDto song)
+    {
+        
+    }
+    
+}
