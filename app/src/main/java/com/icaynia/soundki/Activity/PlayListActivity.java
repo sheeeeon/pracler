604395c6e4e07368a5fba58ee45f36c1c8352f59
icaynia <icaynia@icaynia.com>
2017-02-15 23:23:58 +0900

    Add playlistactivity layout

--- app/src/main/java/com/icaynia/soundki/Activity/PlayListActivity.java
+++ app/src/main/java/com/icaynia/soundki/Activity/PlayListActivity.java
@@ -1,8 +1,29 @@
 package com.icaynia.soundki.Activity;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.icaynia.soundki.R;
+
 /**
  * Created by icaynia on 15/02/2017.
  */
 
-public class PlayListActivity
-{}
+public class PlayListActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+        initializeView();
+    }
+
+    public void initializeView()
+    {
+
+    }
+
+
+
+}
