30afa4cac8d369fc485181eb56b1f0bbf814bca5
icaynia <icaynia@icaynia.com>
2017-02-21 14:11:18 +0900

    Fix issues #6

--- app/src/main/java/com/icaynia/soundki/Activity/PlayListActivity.java
+++ app/src/main/java/com/icaynia/soundki/Activity/PlayListActivity.java
@@ -6,7 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.icaynia.soundki.Global;
@@ -62,8 +65,35 @@ public class PlayListActivity extends AppCompatActivity
             adapter = new PlayListAdapter(this, global.nowPlayingList);
 
             mainListView.setAdapter(adapter);
+            setListViewHeightBasedOnChildren(mainListView);
 
 
         }
     }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            //listItem.measure(0, 0);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+
+        params.height = totalHeight;
+        listView.setLayoutParams(params);
+
+        listView.requestLayout();
+    }
+
+
 }
