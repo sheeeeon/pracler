eb4a6e01dee5c37be2c16c1ccff43214ce053b47
icaynia <icaynia@icaynia.com>
2017-02-21 14:16:23 +0900

    Connect data to playlistactivity

--- app/src/main/java/com/icaynia/soundki/View/PlayListAdapter.java
+++ app/src/main/java/com/icaynia/soundki/View/PlayListAdapter.java
@@ -1,6 +1,9 @@
 package com.icaynia.soundki.View;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.icaynia.soundki.Data.MusicFileManager;
+import com.icaynia.soundki.Global;
 import com.icaynia.soundki.Model.MusicDto;
 import com.icaynia.soundki.Model.PlayList;
 import com.icaynia.soundki.R;
@@ -25,11 +30,14 @@ public class PlayListAdapter extends BaseAdapter
     public PlayList list;
     private LayoutInflater inflater;
 
+    private Global global;
+
     public PlayListAdapter(Context context, PlayList list)
     {
         this.context = context;
         this.list = list;
         inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        global = (Global) context.getApplicationContext();
     }
 
     @Override
@@ -60,7 +68,63 @@ public class PlayListAdapter extends BaseAdapter
             convertView.setLayoutParams(layoutParams);
         }
 
+        ImageView album = (ImageView) convertView.findViewById(R.id.view_album);
+        album.setImageDrawable(context.getResources().getDrawable(android.R.drawable.ic_menu_report_image));
+        TextView title = (TextView) convertView.findViewById(R.id.view_title);
+        TextView artist = (TextView) convertView.findViewById(R.id.view_artist);
+
+        title.setText(list.get(position).title);
+        artist.setText(list.get(position).artist + " - " + list.get(position).album + " - " + global.mMusicManager.convertToTime(list.get(position).length));
+
+        convertView.setTag(list.get(position).id);
+        MyAsyncTask myAsyncTask = new MyAsyncTask();
+        myAsyncTask.setImgView(album);
+        myAsyncTask.execute(position + "");
+
 
         return convertView;
+    }
+
+    public class MyAsyncTask extends AsyncTask<String, Void, Bitmap>
+    {
+        private static final String TAG = "AlbumImageTask";
+        private int position;
+        private ImageView imgView;
+        private MusicFileManager mMusicFileManager = new MusicFileManager(context);
+
+        public void setImgView(ImageView _imgView)
+        {
+            this.imgView = _imgView;
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            Log.i(TAG, "PreExecute");
+            mMusicFileManager = new MusicFileManager(context);
+
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... id)
+        {
+            Bitmap bitmap = null;
+            for (String i : id)
+            {
+                this.position = Integer.parseInt(i);
+                bitmap = mMusicFileManager.getAlbumImage(context, Integer.parseInt(list.get(position).albumid), 100);
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result)
+        {
+            super.onPostExecute(result);
+
+            imgView.setImageBitmap(result);
+        }
+
     }
 }
