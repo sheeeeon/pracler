2cafde17f73006731b998ca395da5b8a8962eb01
icaynia <icaynia@icaynia.com>
2017-02-27 14:54:57 +0900

    Add facebook login

--- app/src/main/java/com/icaynia/soundki/Activity/Splash.java
+++ app/src/main/java/com/icaynia/soundki/Activity/Splash.java
@@ -3,9 +3,24 @@ package com.icaynia.soundki.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 import com.icaynia.soundki.R;
 
 /**
@@ -14,13 +29,42 @@ import com.icaynia.soundki.R;
 
 public class Splash extends AppCompatActivity
 {
-    private LinearLayout facebookLoginButton;
+    private LoginButton facebookLoginButton;
+    private CallbackManager mCallbackManager;
+    private FirebaseAuth mAuth;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        facebookLoginButton = (LinearLayout) findViewById(R.id.facebook_login_button);
+
+        mCallbackManager = CallbackManager.Factory.create();
+        mAuth = FirebaseAuth.getInstance();
+
+        facebookLoginButton = (LoginButton) findViewById(R.id.login_button);
+        facebookLoginButton.setReadPermissions("email", "public_profile");
+        facebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                Log.d("facebook", "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel()
+            {
+                Log.d("facebook", "facebook:cancel:");
+
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+                Log.d("facebook", "facebook:error" + error.toString());
+
+            }
+        });
 
 
         Handler handler = new Handler();
@@ -28,17 +72,45 @@ public class Splash extends AppCompatActivity
             @Override
             public void run()
             {
+                if (mAuth.getCurrentUser().getUid() != null)
+                {
+                    onMainActivity();
+                }
                 facebookLoginButton.setVisibility(LinearLayout.VISIBLE);
-                //onMainActivity();
             }
         }, 3000);
     }
 
-    public void onFacebookLoginButton()
-    {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("Facebook", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Facebook", "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("Facebook", "signInWithCredential", task.getException());
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
 
     public void onMainActivity()
     {
