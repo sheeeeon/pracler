ba187964f38026ef9613736e10542c10b96f1079
icaynia <icaynia@icaynia.com>
2017-03-04 14:14:19 +0900

    Add loadPlaylist method

--- app/src/main/java/com/icaynia/soundki/Data/FileManager.java
+++ app/src/main/java/com/icaynia/soundki/Data/FileManager.java
@@ -1,6 +1,7 @@
 package com.icaynia.soundki.Data;
 
 import android.content.Context;
+import android.media.audiofx.BassBoost;
 import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
@@ -8,8 +9,11 @@ import android.widget.Toast;
 import com.icaynia.soundki.Model.PlayList;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 /**
@@ -43,27 +47,53 @@ public class FileManager
 
     public void savePlaylist(PlayList playlist)
     {
-        String filename = playlist.getName();
-        if (isFileAvailable(filename))
-        {
-            Log.e(TAG, "File is already created");
-            return;
-        }
         if (playlist.getName().equals(""))
         {
             Log.e(TAG, "Playlist name isn't set.");
+            return;
+        }
+
+        String filename = dirPath+"/playlist/"+playlist.getName()+".soundki";
+        if (isFileAvailable(filename))
+        {
+            Log.e(TAG, "File is already created : " + filename);
+            return;
         }
 
         try
         {
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("dg.ser"));
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename));
             oos.writeObject(playlist);
             oos.close();
         }
         catch (Exception e)
         {
+            Log.e(TAG, "Something was wrong!");
             e.printStackTrace();
         }
+    }
+
+    public PlayList loadPlayList(String playlistName)
+    {
+        if (playlistName.equals(""))
+        {
+            Log.e(TAG, "Playlist name isn't set.");
+        }
+        PlayList playlist = null;
+        String filename = dirPath+"/playlist/"+playlistName+".soundki";
+        try
+        {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename));
+            playlist = (PlayList) ois.readObject();
+            ois.close();
+        }
+        catch (Exception e)
+        {
+            Log.e(TAG, "Something was wrong!");
+            e.printStackTrace();
+        }
+
+        return playlist;
     }
 
     public boolean isFileAvailable(String file)
