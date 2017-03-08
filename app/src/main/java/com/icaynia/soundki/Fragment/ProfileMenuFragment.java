5dc23bbd03bff133cb606ebb0e5c1a2dd466ab6a
icaynia <icaynia@icaynia.com>
2017-03-08 10:40:47 +0900

    Add method get image from url

--- app/src/main/java/com/icaynia/soundki/Fragment/ProfileMenuFragment.java
+++ app/src/main/java/com/icaynia/soundki/Fragment/ProfileMenuFragment.java
@@ -1,26 +1,29 @@
 package com.icaynia.soundki.Fragment;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.icaynia.soundki.Activity.ProfileActivity;
-import com.icaynia.soundki.Data.RemoteDatabaseManager;
 import com.icaynia.soundki.Global;
 import com.icaynia.soundki.R;
-import com.icaynia.soundki.View.ProfileMenuAdapter;
 import com.icaynia.soundki.View.ProfileMenuHeader;
+import com.icaynia.soundki.View.ProfileRow;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by icaynia on 2017. 2. 8..
@@ -30,11 +33,13 @@ public class ProfileMenuFragment extends Fragment
 {
     private Global global;
     private View v;
+    private LinearLayout container1;
 
-    private ListView listView;
-    private ProfileMenuHeader profileMenuHeader;
+    private Handler handler = new Handler();
 
-    private ProfileMenuAdapter profileMenuAdapter;
+    private String userPhotoUrl;
+
+    private FirebaseUser firebaseUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -44,35 +49,15 @@ public class ProfileMenuFragment extends Fragment
 
         global = (Global) getContext().getApplicationContext();
 
-        FirebaseUser firebaseUser = global.loginUser;
+        firebaseUser = global.loginUser;
         Log.e("looog", firebaseUser.getDisplayName());
         Log.e("looog", firebaseUser.getPhotoUrl().toString());
         Log.e("looog", firebaseUser.getEmail());
         Log.e("looog", firebaseUser.getProviders().toString());
+        userPhotoUrl = firebaseUser.getPhotoUrl().toString();
 
-        profileMenuHeader = new ProfileMenuHeader(getContext());
-        listView = (ListView) v.findViewById(R.id.profilemenu_list);
-        listView.addHeaderView(profileMenuHeader);
-
-        ArrayList<String> menuString = new ArrayList<>();
-        menuString.add(firebaseUser.getDisplayName());
-        menuString.add("menu 2");
-        menuString.add("menu 3");
-
-        ProfileMenuAdapter profileMenuAdapter = new ProfileMenuAdapter(getContext(), menuString);
-
-        listView.setAdapter(profileMenuAdapter);
-        final String uid = firebaseUser.getUid();
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                if (position == 1) // profile
-                {
-                    onProfileActivity(uid);
-                }
-            }
-        });
+        container1 = (LinearLayout) v.findViewById(R.id.container);
+        listInitialize();
 
         return v;
     }
@@ -83,5 +68,50 @@ public class ProfileMenuFragment extends Fragment
         intent.putExtra("targetUid", uid);
         startActivity(intent);
     }
+
+    private void listInitialize()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            ProfileRow profileRow = new ProfileRow(getContext());
+            getImage(profileRow.imageView, userPhotoUrl);
+            profileRow.setText(firebaseUser.getDisplayName());
+            container1.addView(profileRow);
+        }
+    }
+
+    private void getImage(final ImageView imageView, final String ul)
+    {
+        Thread t = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    URL url = new URL(ul);
+                    InputStream is = url.openStream();
+                    final Bitmap bm = BitmapFactory.decodeStream(is);
+                    handler.post(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {  // 화면에 그려줄 작업
+                            imageView.setImageBitmap(bm);
+                        }
+                    });
+                    imageView.setImageBitmap(bm); //비트맵 객체로 보여주기
+                }
+                catch(Exception e)
+                {
+
+                }
+
+            }
+        });
+
+        t.start();
+    }
+
 
 }
