12f9d5dd52923aa5b4139600e09cedc2ff073e35
icaynia <icaynia@icaynia.com>
2017-03-27 12:04:51 +0900

    Fix starting profile activity

--- app/src/main/java/com/icaynia/soundki/View/FindUserAdapter.java
+++ app/src/main/java/com/icaynia/soundki/View/FindUserAdapter.java
@@ -83,7 +83,7 @@ public class FindUserAdapter extends BaseAdapter
             @Override
             public void onClick(View view)
             {
-                listener.onClick("position", position);
+                listener.onClick("profile", position);
             }
         });
 
