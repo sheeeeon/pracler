3fa5cefce779a0f67beda61121d74645329701aa
icaynia <icaynia@icaynia.com>
2017-03-29 09:59:45 +0900

    Add Song model

---
+++ pracler/src/main/java/com/icaynia/pracler/models/Song.java
@@ -0,0 +1,16 @@
+package com.icaynia.pracler.models;
+
+/**
+ * Created by icaynia on 29/03/2017.
+ */
+
+public class Song
+{
+    public long uid;
+    public long artistUid;
+    public long albumUid;
+    public String title;
+    public String artist;
+    public String album;
+    public String length;
+}
