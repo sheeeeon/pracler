5bce92a4a77f4905326948744cd23a430a31d4b4
icaynia <icaynia@icaynia.com>
2017-04-01 11:40:48 +0900

    Fix bugs lock screen service start lock screen activity intent when it's not playing

--- pracler/src/main/java/com/icaynia/pracler/lockscreen/LockScreenService.java
+++ pracler/src/main/java/com/icaynia/pracler/lockscreen/LockScreenService.java
@@ -7,19 +7,27 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 
+import com.icaynia.pracler.Global;
+
 /**
  * Created by icaynia on 31/03/2017.
  */
 
 public class LockScreenService extends Service
 {
+    private Global global;
     public BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
-                Intent i = new Intent(context, LockScreenActivity.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(i);
+            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction()))
+            {
+                // 음악이 실행 중일때만 사용합니다.
+                if (global.musicService.isPlaying())
+                {
+                    Intent i = new Intent(context, LockScreenActivity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(i);
+                }
             }
         }
     };
@@ -32,6 +40,7 @@ public class LockScreenService extends Service
     @Override
     public void onCreate() {
         super.onCreate();
+        global = (Global) getApplication();
         IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
         registerReceiver(receiver, filter);
     }
